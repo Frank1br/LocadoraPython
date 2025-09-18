fe243b8cf551a6dd589092452f2d6a385776078f
Frank Oliveira <fm0325836@gmail.com>
2025-09-18 16:37:36 -0300

    feat: Estrutura dos metodos

---
+++ Locadora.py
@@ -0,0 +1,26 @@
+from Filme import Filme
+from Cliente import Cliente
+
+class Locadora:
+    def __init__(self):
+        self.filmes = [],
+        self.clientes = []
+
+    def cadastrar_filme(self, filme):
+        pass
+
+    def cadastrar_cliente(self, cliente):
+        pass
+
+    def listar_filmes_disponiveis(self):
+        pass
+
+    def lista_cliente(self):
+        pass
+
+    def alugar_filme(cpf_cliente, codigo_filme):
+        pass
+
+    def devolver_filme(cpf_cliente, codigo_filme):
+        pass
+    
