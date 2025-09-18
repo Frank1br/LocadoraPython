1a1b58d438656f7e5cc373435f94e8429a98c4f4
Frank Oliveira <fm0325836@gmail.com>
2025-09-18 16:27:43 -0300

    feat: criacao das classes

---
+++ Filme.py
@@ -0,0 +1,9 @@
+class Filme:
+    def __init__(self, titulo, genero, ano, codigo, disponivel=True):
+        self.titulo = titulo,
+        self.genero = genero,
+        self.ano = ano,
+        self.codigo = codigo,
+        self.disponivel = disponivel
+
+    
